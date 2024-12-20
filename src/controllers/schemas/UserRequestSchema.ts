9fd1f39e7d903a3a4b9f4ab30958a1c2d119925d
Kayky Dion <kayky.dion@gmail.com>
2024-12-20 07:56:56 -0300

    feat: add zod email validation

--- src/controllers/schemas/UserRequestSchema.ts
+++ src/controllers/schemas/UserRequestSchema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const CreateUserRequestSchema = z.object({
   name: z.string().min(3).max(30),
-  email: z.string().min(6),
+  email: z.string().email(),
   password: z.string().min(8),
 });
 
@@ -17,6 +17,6 @@ export const UpdateUserRequestSchema = z.object({
 });
 
 export const LoginUserRequestSchema = z.object({
-  email: z.string(),
+  email: z.string().email(),
   password: z.string(),
 });
